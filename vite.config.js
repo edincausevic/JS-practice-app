7aa1f384163113bbf9b0c4b8db59c7aa661564d8
edincausevic <edincausevic.p@gmail.com>
2026-07-07 15:01:05 +0200

    link fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/JS-praktice-app/',
+  base: '/JS-practice-app/',
 })
