3d61546164a83dc5dfec28f6982613eb2eb40daa
edincausevic <edincausevic.p@gmail.com>
2026-07-27 19:36:24 +0200

    link to my promotion  app added

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -11,9 +11,9 @@ const MainNav = ({displayExercise, data}) => {
           <h2>📘 JavaScript Drills </h2>
         </div>
         <div className="course-link">
-          <a href="https://www.udemy.com/course/modern-javascript-es6-for-react-nextjs-vue-nodejs/?referralCode=6963EBB03103EBCEF1F8" target="_blank" rel="noopener" style={{marginRight: '7px'}}>
+          <a href="https://edincausevic.github.io/course_promotions/" target="_blank" rel="noopener" style={{marginRight: '7px'}}>
           <span><img style={{height: '15px'}} src={UdemyLogo}/></span> 
-          Go to JS Course</a>
+          My Courses</a>
           <a 
             href="https://www.youtube.com/channel/UCxsJ-g3ALVzsn_VCkKfDXxA" 
             target="_blank" 
